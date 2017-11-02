2c2bc90e98cc15c3d1a999bf7f2fb1578b569bfe
Fulvio PALEARI <fulvio.paleari@uni.lu>
2017-11-02 15:40:19 +0100

    Class YamboOccupationsDB to read occupations in the case of pumped systems

--- yambopy/__init__.py
+++ yambopy/__init__.py
@@ -21,6 +21,7 @@ Modules:
         - YamboElectronsDB: read the electronic states from ns.db1
         - YamboQPDB: read the quasiparticle energies db ndb.QP
         - YamboGreenDB: read the green's functions calculated using yambo
+        - YamboOccupationsDB: read the carrier occupations from a pumped system
 
     bse
         - YamboExcitonWaveFunctionXSF: read the excitonic
@@ -51,6 +52,7 @@ from yambopy.dbs.rtdb import *
 from yambopy.dbs.excitondb import *
 from yambopy.dbs.wfdb import *
 from yambopy.dbs.elphondb import *
+from yambopy.dbs.occupationsdb import *
 
 #input/output files
 from yambopy.io.inputfile import *
